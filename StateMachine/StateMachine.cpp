e2d5c177115dc737cd03e9a9eaa36fb025de8e7d
max <maxim.k.elasnce@gmail.com>
2022-10-04 14:02:05 +0300

    First commit

---
+++ StateMachine/StateMachine.cpp
@@ -0,0 +1,12 @@
+﻿// StateMachine.cpp: определяет точку входа для приложения.
+//
+
+#include "StateMachine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
